e7d1420de0de67414a803193ec94d256930ae08a
Rodolfo Viana <25807981+rodolfo-viana@users.noreply.github.com>
2024-05-17 16:17:49 -0300

    Added script

--- content/aulas/scraping-py/aula4_scraper_ssp_selenium.py
+++ content/aulas/scraping-py/aula4_scraper_ssp_selenium.py
@@ -3,37 +3,34 @@ from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup as bs
 import time
 
-url = "http://www.ssp.sp.gov.br/transparenciassp/Consulta2022.aspx"
+url = "https://www.ssp.sp.gov.br/estatistica/dados-mensais"
 
 # abro o navegador Chrome
 driver = webdriver.Chrome()
 
 # passo a URL para visitar
-driver.get(url) 
+driver.get(url)
 
-# espero 5 segundos para a página carregar
-time.sleep(5) 
-
-# clico no elemento com id cphBody_btnFurtoVeiculo
-driver.find_element(By.XPATH,'//*[@id="cphBody_btnFurtoVeiculo"]').click()
-
-# espero 3 segundos
+# espero 3 segundos para a página carregar
 time.sleep(3)
 
-# clico no elemento com id cphBody_lkAno22
-driver.find_element(By.XPATH,'//*[@id="cphBody_lkAno22"]').click()
+# clico no XPath referente à aba "Taxa de Delito"
+driver.find_element(
+    By.XPATH, '/html/body/app-root/body/div[1]/div/app-dados-mensais/div[2]/div[1]/ul/li[1]/a').click()
 
-# espero 3 segundos
-time.sleep(3)
+# espero 2 segundos
+time.sleep(2)
 
-# clico no elemento com id cphBody_lkMes4
-driver.find_element(By.XPATH,'//*[@id="cphBody_lkMes4"]').click()
+# clico no XPath referente ao município de "Campinas"
+driver.find_element(
+    By.XPATH, '/html/body/app-root/body/div[1]/div/app-dados-mensais/div[2]/div[2]/form/div[2]/div[1]/select/option[110]').click()
 
-# espero 3 segundos
-time.sleep(3)
+# espero 2 segundos
+time.sleep(2)
 
 # passo o código-fonte para Beautiful Soup trabalhar
 soup = bs(driver.page_source, "html.parser")
-dados = soup.find('table', {'id': 'cphBody_grdListBO'})
-with open("cf_selenium.txt", "w", encoding='utf-8') as f:
+dados = soup.find(
+    'table', class_='table table-striped table-hover ng-star-inserted')
+with open("ssp_selenium.txt", "w", encoding='utf-8') as f:
     f.write(str(dados))
